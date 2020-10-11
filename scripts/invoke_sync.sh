068869963867cd4402b143fc063ff4036e5324ed
jeremyj563 <modernclassicgamer@gmail.com>
2020-10-11 21:20:59 +0000

    added gdrive sync commands to script

--- scripts/invoke_sync.sh
+++ scripts/invoke_sync.sh
@@ -4,6 +4,12 @@ if pgrep -x "rclone" > /dev/null; then
   exit 1
 fi
 
+# Google Photos
 rclone sync lyd-gphotos:media/by-year /mnt/fs2/Google/Lydia/Photos/ --gphotos-read-only
 rclone sync jer-gphotos:media/by-year /mnt/fs2/Google/Jeremy/Photos/ --gphotos-read-only
 rclone sync jl-gphotos:media/by-year /mnt/fs2/Google/J\&L/Photos/ --gphotos-read-only
+
+# Google Drive
+rclone sync jer-gdrive:/ /mnt/fs2/Google/Jeremy/Drive/ --drive-auth-owner-only
+rclone sync jl-gdrive:/ /mnt/fs2/Google/J\&L/Drive/ --drive-auth-owner-only
+rclone sync lyd-gdrive:/ /mnt/fs2/Google/Lydia/Drive/ --drive-auth-owner-only
