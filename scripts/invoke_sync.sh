fc892e9b349866431b69695122dbcf44616e6eac
jeremyj563 <modernclassicgamer@gmail.com>
2021-08-22 18:53:48 +0000

    now using the rclone 'copy' command instead of 'sync'

--- scripts/invoke_sync.sh
+++ scripts/invoke_sync.sh
@@ -5,11 +5,11 @@ if pgrep -x "rclone" > /dev/null; then
 fi
 
 # Google Photos
-rclone sync lyd-gphotos:media/by-year /mnt/fs2/Google/Lydia/Photos/ --gphotos-read-only --transfers=1
-rclone sync jer-gphotos:media/by-year /mnt/fs2/Google/Jeremy/Photos/ --gphotos-read-only --transfers=1
-rclone sync jl-gphotos:media/by-year /mnt/fs2/Google/J\&L/Photos/ --gphotos-read-only --transfers=1
+rclone copy lyd-gphotos:media/by-year /mnt/fs2/Google/Lydia/Photos/ --gphotos-read-only --transfers=1
+rclone copy jer-gphotos:media/by-year /mnt/fs2/Google/Jeremy/Photos/ --gphotos-read-only --transfers=1
+rclone copy jl-gphotos:media/by-year /mnt/fs2/Google/J\&L/Photos/ --gphotos-read-only --transfers=1
 
 # Google Drive
-rclone sync jer-gdrive:/ /mnt/fs2/Google/Jeremy/Drive/ --drive-auth-owner-only --transfers=1
-rclone sync jl-gdrive:/ /mnt/fs2/Google/J\&L/Drive/ --drive-auth-owner-only --transfers=1
-rclone sync lyd-gdrive:/ /mnt/fs2/Google/Lydia/Drive/ --drive-auth-owner-only --transfers=1
+rclone copy jer-gdrive:/ /mnt/fs2/Google/Jeremy/Drive/ --drive-auth-owner-only --transfers=1
+rclone copy jl-gdrive:/ /mnt/fs2/Google/J\&L/Drive/ --drive-auth-owner-only --transfers=1
+rclone copy lyd-gdrive:/ /mnt/fs2/Google/Lydia/Drive/ --drive-auth-owner-only --transfers=1
